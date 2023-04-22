a4e618cc39d3d8348c0686e67662240503b14757
lewardo <lewardo23@gmail.com>
2023-04-22 15:05:08 +0100

    toggle glitch, element glitch, global background, mutation handling

--- src/components/Glitched.vue
+++ src/components/Glitched.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :id="id" class="glitch" ref="wrapper">
+    <div :id="id" class="glitch glitching" ref="wrapper">
         <slot/>
     </div>
 </template>
@@ -10,6 +10,7 @@
                 DOMStyleObject: null,
                 DOMStyleKeyframesObject: null,
                 observer: null,
+                content: null,
             }
         },
         props: {
@@ -17,17 +18,21 @@
                 type: String,
                 default: 'glitchy',
             },
+            start: {
+                type: Boolean,
+                default: true,
+            },
             glitch: {
                 type: String,
                 defualt: '',
             },
             fg: {
                 type: String,
-                default: 'black',
+                default: 'var(--glitch-global-fg, #fff)',
             },
             bg: {
                 type: String,
-                default: 'white',
+                default: 'var(--glitch-global-bg, #000)',
             },
             intensity: {
                 type: Number,
@@ -39,20 +44,32 @@
             }
         },
         methods: {
+            stopGlitching: function() {
+                document.getElementById(this.id).classList.remove('glitching');
+            },
+            startGlitching: function() {
+                document.getElementById(this.id).classList.add('glitching');
+            },
+            toggleGlitching: function() {
+                document.getElementById(this.id).classList.toggle('glitching');
+            },
             generateKeyframes: function() {
-                let keyframes1 = '', keyframes2 = '';
+                let keyframesBefore = '', keyframesAfter = '', keyframesEl = '';
 
                 for(let i = 0; i < this.steps; i++) {
-                    keyframes1 += `${100 * i / this.steps}%{clip-path:inset(${Math.random() > this.intensity ? 100 : Math.round(Math.random() * 100)}% 0 ${Math.round(Math.random() * 100)}% 0);}`
-                    keyframes2 += `${100 * i / this.steps}%{clip-path:inset(${Math.random() > this.intensity ? 100 : Math.round(Math.random() * 100)}% 0 ${Math.round(Math.random() * 100)}% 0);}`
+                    keyframesBefore += `${100 * i / this.steps}%{clip-path:inset(${Math.random() * 3 > this.intensity * 2 ? 100 : Math.round(Math.random() * 100)}% 0 ${Math.round(Math.random() * 100)}% 0);}`;
+                    keyframesAfter += `${100 * i / this.steps}%{clip-path:inset(${Math.random() * 3 > this.intensity * 2 ? 100 : Math.round(Math.random() * 100)}% 0 ${Math.round(Math.random() * 100)}% 0);}`;
+                    keyframesEl += `${100 * i / this.steps}%{clip-path:inset(${Math.random() * 25 > this.intensity ? 0 : 50}% 0 ${Math.random() * 25 > this.intensity ? 0 : 25}% 0);}`
                 }
 
-                return `@-webkit-keyframes noise-anim-${this.id}-1 {${keyframes1}}@keyframes noise-anim-${this.id}-1 {${keyframes1}}@-webkit-keyframes noise-anim-${this.id}-2 {${keyframes2}}@keyframes noise-anim-${this.id}-2 {${keyframes2}}`
+                return `@-webkit-keyframes noise-anim-${this.id}{${keyframesEl}}@keyframes noise-anim-${this.id}{${keyframesEl}}@-webkit-keyframes noise-anim-${this.id}-before{${keyframesBefore}}@keyframes noise-anim-${this.id}-before{${keyframesBefore}}@-webkit-keyframes noise-anim-${this.id}-after{${keyframesAfter}}@keyframes noise-anim-${this.id}-after{${keyframesAfter}}`
             },
             regurgitateStyling: function() {
-                this.slotText = this.glitch || this.$refs.wrapper.innerText;
+                const elDuraion = 7.9 + Math.random(), beforeDuration = 3.7 + Math.random(), afterDuration = 4.1 + Math.random();
 
-                return `#${this.id}.glitch{color:${this.fg};}#${this.id}.glitch:before{content:"${this.slotText}";-webkit-animation:noise-anim-${this.id}-2 3.7s infinite steps(2,end) alternate-reverse;animation:noise-anim-${this.id}-2 3.7s infinite steps(2,end) alternate-reverse;color:${this.fg};background:${this.bg};text-shadow:-1px 0px ${this.fg};}#${this.id}.glitch:after{content:"${this.slotText}";-webkit-animation:noise-anim-${this.id}-1 4.1s infinite steps(2,end) alternate-reverse;animation:noise-anim-${this.id}-1 4.1s infinite steps(2,end) alternate-reverse;color:${this.fg};background:${this.bg};text-shadow:1px 0 ${this.fg};}`
+                this.content = this.glitch || this.$refs.wrapper.innerText;
+
+                return `#${this.id}.glitching{-webkit-animation:noise-anim-${this.id} ${elDuraion}s infinite step-end alternate-reverse;animation:noise-anim-${this.id} ${elDuraion}s infinite step-end alternate-reverse;}#${this.id}.glitch{color:${this.fg};}#${this.id}.glitching::before{-webkit-animation:noise-anim-${this.id}-before ${beforeDuration}s infinite step-end alternate-reverse;animation:noise-anim-${this.id}-before ${beforeDuration}s infinite step-end alternate-reverse;}#${this.id}.glitch::before{content:"${this.content}";color:${this.fg};background:${this.bg};text-shadow:-1px 0px ${this.fg};}#${this.id}.glitching::after{-webkit-animation:noise-anim-${this.id}-after ${afterDuration} infinite step-end alternate-reverse;animation:noise-anim-${this.id}-after ${afterDuration}s infinite step-end alternate-reverse;}#${this.id}.glitch::after{content:"${this.content}";color:${this.fg};background:${this.bg};text-shadow:1px 0 ${this.fg};}`
             }
         },
         mounted: function() {
@@ -61,10 +78,6 @@
 
             this.DOMStyleObject = document.createElement('style');
             this.DOMStyleKeyframesObject = document.createElement('style');
-
-            this.observer = new MutationObserver(function(mutations) {
-                this.DOMStyleObject.innerHTML = this.regurgitateStyling();
-            }.bind(this));
 
             this.DOMStyleObject.id = `${this.id}-style`;
             this.DOMStyleObject.innerHTML = componentStyle;
@@ -75,14 +88,22 @@
             document.head.appendChild(this.DOMStyleObject);
             document.head.appendChild(this.DOMStyleKeyframesObject);
 
-            this.observer.observe(this.$el, { 
-                characterData: true, 
-                childList: true,
-                subtree: true,
-            });
+            if(this.glitch !== '') {
+                this.observer = new MutationObserver(function(mutations) {
+                    this.DOMStyleObject.innerHTML = this.regurgitateStyling();
+                }.bind(this));
+
+                this.observer.observe(this.$el, { 
+                    characterData: true, 
+                    childList: true,
+                    subtree: true,
+                });
+            }
         },
         beforeDestroy: function() {
-            this.observer.disconnect();
+            if(this.glitch !== '') {
+                this.observer.disconnect();
+            }
         }
     }
 </script>
@@ -90,20 +111,22 @@
      .glitch {
         position: relative;
         overflow: hidden;
+        white-space: nowrap;
      }
 
-     .glitch:before, .glitch:after {
+     .glitch::before, .glitch::after {
         position: absolute;
+        user-select: none;
         top: 0;
         overflow: hidden;
-        clip-path: inset(0 0 0 0);
+        clip-path: inset(100% 0 0 0);
      }
 
-     .glitch:before {
+     .glitch::before {
         left: -1px;
      }
 
-     .glitch:after {
+     .glitch::after {
         left: 1px;
      }
 </style>
