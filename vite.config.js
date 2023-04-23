c553696fd765f7369830c3e61fd24ad817031ef8
lewardo <lewardo23@gmail.com>
2023-04-23 19:15:34 +0100

    docs building option

--- vite.config.js
+++ vite.config.js
@@ -4,22 +4,44 @@ import vue from '@vitejs/plugin-vue'
 
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/components/index.js'),
-      name: 'vue-glitched',
-      formats: ["es", "umd"],
-      filename: format => `vue-glitched.${format}.js`
-    },
-    rollupOptions: {
-      external: ['vue'],
-      output: {
-        globals: {
-          vue: 'Vue',
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if(mode === 'docs') {
+    return {
+      plugins: [vue()],
+      build: {
+        minify: true,
+        outDir: 'docs',
+        rollupOptions: {
+          input: {
+            index: resolve(__dirname, 'index.html'),
+          },
+          output: {
+            entryFileNames: 'assets/[name].js',
+            chunkFileNames: 'assets/[name].js',
+            assetFileNames: 'assets/[name].[ext]',
+          }
+        }
+      }
+    }
+  } else {
+    return {
+      build: {
+        lib: {
+          entry: resolve(__dirname, 'src/components/index.js'),
+          name: 'vue-glitched',
+          formats: ["es", "umd"],
+          filename: format => `vue-glitched.${format}.js`
+        },
+        rollupOptions: {
+          external: ['vue'],
+          output: {
+            globals: {
+              vue: 'Vue',
+            },
+          },
         },
       },
-    },
-  },
-  plugins: [vue()],
+      plugins: [vue()],
+    }
+  }
 })
