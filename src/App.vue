47ac341f84a90541eb5608262c582446d0391724
lewardo <lewardo23@gmail.com>
2023-04-23 18:10:17 +0100

    more examples and gruvbox colour

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
-  import { glitch } from './components/';
+  import { Glitch } from './components/';
 </script>
 
 <template>
-  <glitch>simple text glitching vue component</glitch>
-  <glitch id="secondary" :intensity=0.4 fg="#8FBCBB" glitch="notice not the same text is glitching here">because whyn't re-invent the wheel amrite?</glitch>
-  <a href="http://t.ly/qIQs">@lewardo</a>
+  <div class="wrapper">
+    <Glitch id="glitchy" :intensity=0.2>vue3 text glitching component</Glitch>
+    <Glitch id="intense" :intensity=0.9>built with <span style="color: #41b883;">vue</span> &amp; <span style="color: #bd34fe;">vite</span></Glitch>
+    <div>
+      <Glitch id="colourful" bg="#1d2021" fg="#ebdbb2" colour="#928374">simple to use, customisable, and&nbsp;</Glitch>
+      <Glitch id="disabled" :start=true text="different glitching overlay">completely open source <span style="color: #cc241d;">&lt;3</span></Glitch>
+    </div>
+      <a href="http://t.ly/qIQs">@lewardo</a>
+  </div>
 </template>
 
 <style>
@@ -20,29 +26,41 @@
     box-sizing: border-box;
     place-items: center;
     display: grid;
-    background: #2E3440;
+    background: #1d2021;
 
-    --glitch-global-bg: #2E3440;
-    --glitch-global-fg: #D8DEE9;
+    --glitch-global-bg: #1d2021;
+    --glitch-global-fg: #ebdbb2;
+  }
+
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
   }
 
   .glitch {
     font-family: monospace;
+    display: block;
   }
 
   #glitchy {
     font-size: 48px;
+    margin: 20px 0;
   }
 
-  #secondary {
+  #intense, #colourful, #disabled {
     font-size: 24px;
+  }
+
+  #colourful, #disabled {
+    display: inline-block;
   }
   
   a, a:visited {
     margin: 20px;
     font-family: monospace;
     font-size: 20px;
-    color: #5E81AC;
+    color: #928374;
     text-decoration: none;
     position: absolute;
     bottom: 0;
